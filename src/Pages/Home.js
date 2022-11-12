30b3f6e6859c27eee5f8bce3603215441e9a75f4
Fernanda Avalos <fernanda.avalos2002@gmail.com>
2022-11-12 11:03:38 -0600

    API display

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -6,7 +6,7 @@ import { useFetchProducts } from '../Hooks/useFetchProducts'
 export const Home = () => {
 
   const { products } = useFetchProducts();
-  console.dir(products);
+  //console.dir(products);
   return (
     <div className='container row row-cols-1 row-cols-md-3 g-3'>
       { products.map((product) => 
